3d27554985c1e3109886a2e055cfae75853afd42
spayker <spykerstar@gmail.com>
2021-12-18 21:17:15 +0100

    renamed repo folders, added server resources

---
+++ resources/server/h2g_server.bat
@@ -0,0 +1,3 @@
+@echo off
+echo ArmA 3 Restart Script
+start /wait /high ./arma3server_x64.exe -profiles=Profiles -name=Administrator -port=2302 -bandwidthAlg=2 -limitFPS=200 -hugePages -cfg=config.cfg -config=server.cfg -malloc=system -loadMissionToMemory -world=empty "-mod=!Workshop\@CBA_A3;"
